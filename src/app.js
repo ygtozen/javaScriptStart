44e6d33cfa48f3aa1facefe0431df572711481d8
TCYIOZEN <yigit.ozen@atmosware.turkcell.com.tr>
2024-03-11 18:12:41 +0300

    değişkenler

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 console.log("Merhaba Kodlama.io");
+
+// JS type safe değildir.
+let dolarBugun = 9.30;
+
+let dolarDun = 9.20;
+
+{
+    let dolarDun = 9.10;
+}
+
+console.log(dolarDun);
+
+const euroDun = 11.2
+euroDun = 11
+
+console.log(euroDun);
