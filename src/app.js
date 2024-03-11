811298c736b0fdd9b952c0fbe8ff6e24611501ab
ygtozen <ozenyigit151@gmail.com>
2024-03-11 19:13:10 +0300

    arrayler ve for dongusu

--- src/app.js
+++ src/app.js
@@ -10,8 +10,22 @@ let dolarDun = 9.20;
 }
 
 console.log(dolarDun);
-
 const euroDun = 11.2
-euroDun = 11
+// euroDun = 11 --> const değişkeni sabittir, bir kere tanımlandıktan sonra değerini değiştiremeyiz
 
 console.log(euroDun);
+
+// Array
+// camelCasing
+// PascalCasing
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kresi", "Kamu  Konut Kredisi", "Özel Konut Kredisi"];
+
+console.log("<ul>");
+for(let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>" + konutKredileri[i] + "</li>");
+}
+console.log("</ul>");
+
+console.log(konutKredileri);
+
+// konutKredileri.forEach(str => console.log(str)); // foreach
