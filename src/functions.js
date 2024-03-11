96b9f60a46a243015a4414005b053e5d4c2bc116
ygtozen <ozenyigit151@gmail.com>
2024-03-12 01:01:13 +0300

    functions, arrow function

---
+++ src/functions.js
@@ -0,0 +1,18 @@
+function addToCart(quantity,productName="Elma") {
+    console.log("Sepete eklendi! : " + productName + ", adet : " + quantity);
+}
+
+//addToCart();
+addToCart(10);
+//addToCart("Karpuz");
+
+let dolarDun = 9.50
+let sayHello = ()=> {
+    console.log("Hello World");
+}
+sayHello()
+
+let sayHello2 = function() {
+    console.log("Hello World 2");
+}
+sayHello2(); 
